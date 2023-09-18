3e92d18aeb790d0af5e6ce15c85826a23cedc500
AzinMapar <azin.mapar@yahoo.com>
2023-09-18 10:23:11 +0330

    using bloc across Globally

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,34 @@
+import 'package:bloc_counter/logic/cubit/counter_cubit.dart';
 import 'package:bloc_counter/presentation/router/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  // final CounterCubit _counterCubit = CounterCubit();
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
 
   final AppRouter _appRouter = AppRouter();
+
   @override
   Widget build(BuildContext context) {
     // # wrap with blocProvider<CounterCubit> that returns CounterCubit
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider<CounterCubit>(
+      create: (context) => CounterCubit(),
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        onGenerateRoute: _appRouter.onGenerateRoute,
       ),
-      // routes: const {
-      // # we don't do it like this
-      // '/': (context) => BlocProvider(
-      //       create: (BuildContext context) => _counterCubit,
-      //       child: const HomeScreen(
-      //         title: 'Flutter Demo Home Page',
-      //         color: Colors.blueAccent,
-      //       ),
-      //     ),
-      // # Named Routing
-      // '/': (context) => BlocProvider.value(
-      //       value: _counterCubit,
-      //       child: const HomeScreen(
-      //         title: 'Flutter Demo Home Page',
-      //         color: Colors.blueAccent,
-      //       ),
-      //     ),
-      // '/second': (context) => BlocProvider.value(
-      //       value: _counterCubit,
-      //       child: const SecondScreen(
-      //         title: 'Flutter Demo Second Page',
-      //         color: Colors.redAccent,
-      //       ),
-      //     ),
-      // '/third': (context) => BlocProvider.value(
-      //       value: _counterCubit,
-      //       child: const ThirdScreen(
-      //         title: 'Flutter Demo Third Page',
-      //         color: Colors.greenAccent,
-      //       ),
-      //     ),
-      // },
-      onGenerateRoute: _appRouter.onGenerateRoute,
     );
-  }
-
-  @override
-  void dispose() {
-    // # we should do this manually for Named Routing
-    // _counterCubit.close();
-    _appRouter.dispose();
-    super.dispose();
   }
 }
 
-/* # Access and Navigation
-# when we use blocProvider like this around homeScreen the bloc only can be used inside of HomeScreen and if we navigate to another screen after home screen we can't use instances of bloc
-# we have 3 kinds of routing: 1.Anonymous Routing, 2.Named Routing, 3.Generated Routing
-# 1. for the Anonymous Routing we can solve the problem like it is explained in home_screen.dart
-# 2. for Named Routing in routes we wrap every screen in blocProvider but we don't give them CounterCubit() because then we would have a new instance of cubit for every one of them
-# Instead we create a private instance of CounterCubit at the top and use the same one for all of them
-# BUT THIS IS NOT A GOOD WAY
-# insteadof blocProvider we should use BlocProvider.value
-# 3. for Generated Routing first we create a file named app_router.dart, more info inside that file
+/* # Access and Navigation => Global Access
+# Just Wrap the Material App with BlocProvider
 # */
--- lib/presentation/router/app_router.dart
+++ lib/presentation/router/app_router.dart
@@ -1,52 +1,32 @@
-import 'package:bloc_counter/logic/cubit/counter_cubit.dart';
 import 'package:bloc_counter/presentation/screens/home_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../screens/second_screen.dart';
 import '../screens/third_screen.dart';
 
 class AppRouter {
-  final CounterCubit _counterCubit = CounterCubit();
-
   Route? onGenerateRoute(RouteSettings routeSettings) {
     switch (routeSettings.name) {
       case '/':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-              value: _counterCubit,
-              child: const HomeScreen(
-                  title: 'Home Screen', color: Colors.blueAccent)),
+          builder: (_) =>
+              const HomeScreen(title: 'Home Screen', color: Colors.blueAccent),
         );
 
       case '/second':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: const SecondScreen(
-                title: 'Second Screen', color: Colors.redAccent),
-          ),
+          builder: (_) => const SecondScreen(
+              title: 'Second Screen', color: Colors.redAccent),
         );
 
       case '/third':
         return MaterialPageRoute(
-          builder: (_) => BlocProvider.value(
-            value: _counterCubit,
-            child: const ThirdScreen(
-                title: 'Third Screen', color: Colors.greenAccent),
-          ),
+          builder: (_) => const ThirdScreen(
+              title: 'Third Screen', color: Colors.greenAccent),
         );
 
       default:
         return null;
     }
   }
-
-  void dispose() {
-    _counterCubit.close();
-  }
 }
-
-/* #
-# just same things as Named Routing but in a seperate file
-# */
